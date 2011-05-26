0b58c0ea73d8a1a1ee3bf4561c6041d6f526e7bb
Michael Pasternak <mpastern@redhat.com>
2011-05-26 10:31:31 +0300

    python: promote upstream dependency to 1.0-rc1.0

--- rhevsh/setup.py
+++ rhevsh/setup.py
@@ -15,7 +15,7 @@ from setuptools import setup, Command
 
 version_info = {
     'name': 'rhevsh',
-    'version': '0.9-SNAPSHOT',
+    'version': '1.0-rc1.0',
     'description': 'A command-line interface to Red Hat Enterprise'
                    ' Virtualization',
     'author': 'Geert Jansen',
